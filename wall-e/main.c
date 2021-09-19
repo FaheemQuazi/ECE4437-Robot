4ef454d975097f0b3b5dcb888bd678a31d463ba8
Faheem Quazi <faheemquazi@gmail.com>
2021-09-19 12:52:37 -0500

    (M0) Add base robot project
    - Update .gitignore to be more generic
    - Added Mac-specific system files to .gitignore

---
+++ wall-e/main.c
@@ -0,0 +1,55 @@
+/*
+ *  ======= main ========
+ *
+ *  Created on: Sep 16, 2021
+ *  Author:     Team 1
+ *  Members:    Faheem Quazi, Aruba Bukhari, Caleb Ballard
+ */
+
+/* Standard libs */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Constants */
+#include <inc/hw_ints.h>
+#include <inc/hw_memmap.h>
+
+/* XDC */
+#include <xdc/runtime/System.h>
+
+/* DriverLib */
+#include <driverlib/sysctl.h>
+#include <driverlib/gpio.h>
+
+/* TI-RTOS BIOS  */
+#include <ti/sysbios/BIOS.h>
+
+// *** Board Initialization Function ***
+void Board_Init() {
+    // ****** GPIO Init ******
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+}
+
+/*
+ * ======== main ========
+ */
+Int main(void)
+{
+    Board_Init();
+    BIOS_start();
+    return (0);
+}
+
+// ======== blink LED clock routine ========
+int lightCount = 0;
+Void clkLight(UArg arg0) {
+    if (lightCount % 2 == 0) {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 14);
+    } else {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
+    }
+    if (lightCount++ > 10) {
+        lightCount = 0;
+    }
+}
