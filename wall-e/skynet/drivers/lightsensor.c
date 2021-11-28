aeccfae28a8f81e6c74f01720420ed0df54b0900
aruba235 <78322352+aruba235@users.noreply.github.com>
2021-11-27 19:27:46 -0600

    Update lightsensor.c

--- wall-e/skynet/drivers/lightsensor.c
+++ wall-e/skynet/drivers/lightsensor.c
@@ -39,9 +39,9 @@ void LightSensor_Init(void) {
 
     x = (char*)malloc(sizeof(char) * 16);
 
-   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // Enable PortD Peripheral
-   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Enable PortF Peripheral
-   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1); // Make PortF Pin1 Output
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);                     // Enable PortD Peripheral
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);                     // Enable PortF Peripheral
+   GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);              // Make PortF Pin1 Output
 }
 
 
@@ -54,77 +54,78 @@ void GetLight() {
     switch (state) {
     case 0:
         // set up light sensor
-        GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_3);         // Make PortD pin1 output to charge the sensor
+        GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_3);         // Make PortD pin 3 output to charge the sensor
         GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, GPIO_PIN_3);      // Start charing the sensor
-        if (count < 2) {
+        if (count < 2) {                                            //threshold to start counting up
             count++;
         } else {
-            state = 1;
-            count = 0;
+            state = 1;                                              //jump to case 1
+            count = 0;                                              //reset counter
         }
         break;
     case 1:
-        GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_3);          // Make PortD pin1 input to time the decaying
+        GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_3);          // Make PortD pin 3 input to time the decaying
         pinValue = GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_3);        // Assign the value when sensor is in fully charged state
         if (pinValue & GPIO_PIN_3) {
-            count++;
+            count++;                                                //start counting
         } else {
-            state = 2;
+            state = 2;                                              //jump to case 2
         }
         break;
     case 2:
-        if (count > 2) {
-            isBlack = true;
+        if (count > 2) {                                            //threshold to determine when on black surface
+            isBlack = true;                                         //on black surface
 
         } else {
-            isBlack = false;
+            isBlack = false;                                        //if less than threshold assume on white surface
         }
-        count = 0;
-        state = 3;
+        count = 0;                                                  //reset the counter
+        state = 3;                                                  //jump to case 3
         break;
     case 3:
         if (count < 20) {
-            count++;
+            count++;                                                //continue counting up if threshold is less than 20
         } else {
-            count = 0;
+            count = 0;                                              //if greater than 20 reset the counter
             state = 0;
         }
         break;
     }
-    detectLine();
+    detectLine();                                                   //call function to determine the thickness of the line
 }
 
 int lineCount = 0;
 bool onBlack = false;
 bool sendData = false;
+//function to determine thickness of the black tape
 void detectLine() {
-    if (onBlack == false && isBlack == true) {          //are we over black or white?
-        onBlack = true;                                 //set onBlack = true
+    if (onBlack == false && isBlack == true) {                      //if previous state was white and current state is black
+        onBlack = true;                                             //set previous state to black
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2, 0);
-        lineCount = 1;                                //start counting
+        lineCount = 1;                                              //start counting
     }
 
-    if (onBlack == true && isBlack == true) {
-        lineCount++;                               //keep counting
+    if (onBlack == true && isBlack == true) {                       //if previous state was black and current state is also black
+        lineCount++;                                                //keep counting
         if (lineCount > 20000) {
             lineCount = 20000;
         }
     }
 
-    if (onBlack == true && isBlack == false) {
-        onBlack = false;
-        if (lineCount > 240) {
-            setESTOP();
+    if (onBlack == true && isBlack == false) {                      //if previous state was black and current state is white
+        onBlack = false;                                            //set previous state to white
+        if (lineCount > 240) {                                      //if lineCount exceeds threshold for a single black line
+            setESTOP();                                             //stop the robot
             timestarted = false;
-            float timeinseconds = (float)timesincestart / 4;
-            sprintf(x, "\r\n%0.2fs", timeinseconds);
+            float timeinseconds = (float)timesincestart / 4;        //convert to seconds
+            sprintf(x, "\r\n%0.2fs", timeinseconds);                //print time in seconds from start until the robot stops at thick line
             BT_PrintString(x);
-        } else if (lineCount > 90) {
+        } else if (lineCount > 90) {                                //if encounter thin black line send data
             sendData = !sendData;
             if (sendData) {
                 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 4);
             } else {
-                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+                GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);       //turn off led
             }
         }
     }
